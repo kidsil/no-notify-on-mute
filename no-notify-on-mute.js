8af3e0fab78190df930a2d44d5e5a434cf000211
Asaf Zamir <asaf.zamir@gmail.com>
2020-05-18 20:01:36 +0200

    First Commit

--- no-notify-on-mute.js
+++ no-notify-on-mute.js
@@ -1,34 +1,35 @@
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (typeof changeInfo.mutedInfo !== 'undefined') {
-    const url = new URL(tab.url);
-    
-
-      //Conditions:
-      // if currentPermission exists AND tab has been unmuted -> Change Notification Permission to currentPermission AND remove storage.local entry
-      // if currentPermission exists AND tab has been muted -> Write Notification Permission into storage.local entry AND change Notification Permission to denied
-      // if currentPermission doesn't exist AND tab has been muted -> Write Notification Permission into storage.local entry AND change Notification Permission to denied
-      // if currentPermission doesn't exist AND tab has been unmuted -> Do Nothing.
-      //
-    if (changeInfo.mutedInfo.muted) {
-      const result = await browser.tabs.executeScript(tabId, {code:'Notification.permission'});
-      if (result[0]) {
-        let notifyPermission = {};
-        notifyPermission[url.origin] = result[0];
-        await browser.storage.local.set(notifyPermission);
+    const mutingScript = `
+      if (typeof window.Notification.permission !== 'undefined') {
+        console.log('Disabling Notifications (tab muted)');
+        window.tempMutedNotification = window.Notification;
+        window.Notification = class empty {};
       }
+    `;
 
-      //TODO: change notification permission...
-      console.log('CHANGING NOTIFICATION PERMISSION TO DENIED!');
-      //we need to save the notifications settings somewhere (addon storage of some sorts...)
-      //Notification.permission == 'default' || 'granted' || 'denied'
-    } else {
-      const currentPermission = await browser.storage.local.get(url.origin);
-      console.log('currentPermission');
-      console.log(currentPermission);
-      if (currentPermission[url.origin]) {
-        //TODO: restore notification permission...
-        console.log('RESTORING NOTIFICATION PERMISSION');
-      }
-    }
+    const unmutingScript = `
+        if (window.tempMutedNotification) {
+          console.log('Re-enabling Notifications (tab unmuted)');
+          window.Notification = window.tempMutedNotification;
+          delete window.tempMutedNotification;
+        }
+    `;
+
+    const createAndAppendElem = (mute = true) => {
+      let scriptElemId = mute ? 'tempMutedNotification' : 'tempUnMutedNotification';
+      return `
+        //Check & remove existing element
+        var scriptElem = document.getElementById(\'${scriptElemId}\');
+        if (scriptElem) scriptElem.parentNode.removeChild(scriptElem);
+        //Create and append new element
+        var inj = document.createElement('script');
+        inj.id = \'${scriptElemId}\';
+        inj.innerHTML = \`${mute ? mutingScript : unmutingScript}\`;
+        document.body.appendChild(inj);
+      `;
+    };
+
+    await browser.tabs.executeScript(tabId, {code: createAndAppendElem( changeInfo.mutedInfo.muted )});
   }
 });
